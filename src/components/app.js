17f223d1c584e757d775b5ed9d634d767a464cf6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-30 19:08:50 +0000

    Automatically backed up by Learn

--- src/components/app.js
+++ src/components/app.js
@@ -54,7 +54,7 @@ export default class App extends React.Component{
   render(){
     return(<div>
       <UserBlackjack userCards={this.props.store.getState().userCards} score={this.calculateUserScore} hitMe={this.hitMe} stay={this.stay}/>
-      <AIBlackjack aiCards={this.props.store.getState().aiCards} score={this.calculateAiScore} hitMe={this.hitMe}/>
+      <AIBlackjack aiCards={this.props.store.getState().aiCards} score={this.calculateAiScore} hitMe={this.hitMe} userScore={this.calculateUserScore}/>
       </div>)
   }
 }
