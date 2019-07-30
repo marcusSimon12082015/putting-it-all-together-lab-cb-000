f4ef6796b0c5a1fee07d1db73d4644d64a6537c1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-30 19:20:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import App from './components/app'
 const store = createStore(blackjackReducer);
 
 
-ReactDOM.render(<App store={store}/>, document.getElementById('container'))
+const render = () => {
+  ReactDOM.render(<App store={store}/>, document.getElementById('container'))
+}
 
 store.subscribe(render)
 store.dispatch(fetchDeck())
