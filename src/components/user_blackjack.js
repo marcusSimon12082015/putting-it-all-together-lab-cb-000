9ebad20dca8142cc19433e0cb78ad84e76e9bd97
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-30 18:46:00 +0000

    Automatically backed up by Learn

--- src/components/user_blackjack.js
+++ src/components/user_blackjack.js
@@ -3,12 +3,18 @@ import React from 'react'
 const UserBlackjack = (props) => {
   return(
     <div>
-      <h1>Player1</h1>
-      <h2>Score: {props.score()}</h2>
-      <ul>
-        {props.userCards.map(({name,value})=> <li>{name}</li>)}
-      </ul>
-    </div>
+     <h1>Player1</h1>
+     <h2>Score: {props.score()}</h2>
+     <ul>
+       {props.userCards.map(({name,value})=> <li>{name}</li>)}
+     </ul>
+     <form onSubmit={this.hitMe}>
+       <button type="submit"> Hit Me </button>
+     </form>
+     <form onSubmit={this.stay}>
+       <button type="submit"> Stay </button>
+     </form>
+   </div>
   );
 }
 
